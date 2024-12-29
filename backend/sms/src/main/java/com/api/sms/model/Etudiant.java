9886a3ed17c15f9deb478c3b1321179785a3581b
Chahid Hadifi <chahidhadifi@gmail.com>
2024-12-29 09:59:18 +0100

    feat: add `docker-compose` files

--- backend/sms/src/main/java/com/api/sms/model/Etudiant.java
+++ backend/sms/src/main/java/com/api/sms/model/Etudiant.java
@@ -4,13 +4,8 @@ import java.time.*;
 import java.util.*;
 import com.fasterxml.jackson.annotation.*;
 import jakarta.persistence.*;
-import lombok.*;
 
-@Getter
-@Setter
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
 public class Etudiant {
 
     @Id
@@ -27,4 +22,45 @@ public class Etudiant {
     @JsonManagedReference
     private List<Note> notes;
 
+    public Etudiant() {
+    }
+
+    public Etudiant(Long id, String nom, LocalDateTime dateDeCreation, List<Note> notes) {
+        this.id = id;
+        this.nom = nom;
+        this.dateDeCreation = dateDeCreation;
+        this.notes = notes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public LocalDateTime getDateDeCreation() {
+        return dateDeCreation;
+    }
+
+    public void setDateDeCreation(LocalDateTime dateDeCreation) {
+        this.dateDeCreation = dateDeCreation;
+    }
+
+    public List<Note> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
+    }
 }
--- backend/sms/src/main/java/com/api/sms/model/Note.java
+++ backend/sms/src/main/java/com/api/sms/model/Note.java
@@ -2,13 +2,8 @@ package com.api.sms.model;
 
 import com.fasterxml.jackson.annotation.*;
 import jakarta.persistence.*;
-import lombok.*;
 
-@Getter
-@Setter
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
 public class Note {
 
     @Id
@@ -24,4 +19,45 @@ public class Note {
     @JsonBackReference
     private Etudiant etudiant;
 
+    public Note() {
+    }
+
+    public Note(Long id, String nomDuCours, Double valeurDeNote, Etudiant etudiant) {
+        this.id = id;
+        this.nomDuCours = nomDuCours;
+        this.valeurDeNote = valeurDeNote;
+        this.etudiant = etudiant;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomDuCours() {
+        return nomDuCours;
+    }
+
+    public void setNomDuCours(String nomDuCours) {
+        this.nomDuCours = nomDuCours;
+    }
+
+    public Double getValeurDeNote() {
+        return valeurDeNote;
+    }
+
+    public void setValeurDeNote(Double valeurDeNote) {
+        this.valeurDeNote = valeurDeNote;
+    }
+
+    public Etudiant getEtudiant() {
+        return etudiant;
+    }
+
+    public void setEtudiant(Etudiant etudiant) {
+        this.etudiant = etudiant;
+    }
 }
