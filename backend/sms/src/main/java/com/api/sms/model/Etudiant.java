239d8637d3d55c96ddef26e5d08255ee1786fea6
Chahid Hadifi <chahidhadifi@gmail.com>
2024-12-29 11:30:36 +0100

    feat: add `docker-compose` files

--- backend/sms/src/main/java/com/api/sms/model/Etudiant.java
+++ backend/sms/src/main/java/com/api/sms/model/Etudiant.java
@@ -4,8 +4,13 @@ import java.time.*;
 import java.util.*;
 import com.fasterxml.jackson.annotation.*;
 import jakarta.persistence.*;
+import lombok.*;
 
+@Getter
+@Setter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Etudiant {
 
     @Id
@@ -22,45 +27,4 @@ public class Etudiant {
     @JsonManagedReference
     private List<Note> notes;
 
-    public Etudiant() {
-    }
-
-    public Etudiant(Long id, String nom, LocalDateTime dateDeCreation, List<Note> notes) {
-        this.id = id;
-        this.nom = nom;
-        this.dateDeCreation = dateDeCreation;
-        this.notes = notes;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public LocalDateTime getDateDeCreation() {
-        return dateDeCreation;
-    }
-
-    public void setDateDeCreation(LocalDateTime dateDeCreation) {
-        this.dateDeCreation = dateDeCreation;
-    }
-
-    public List<Note> getNotes() {
-        return notes;
-    }
-
-    public void setNotes(List<Note> notes) {
-        this.notes = notes;
-    }
 }
